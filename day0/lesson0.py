edf197acfceb2ac2c2b1fa10cc8c406d2f710f9d
nikakviciani <kalichavanika55@gmail.com>
2025-02-11 19:22:43 +0400

    Update lesson0.py

--- day0/lesson0.py
+++ day0/lesson0.py
@@ -1,2 +1,68 @@
-print("day 0 lesson") 
-print("hello")
+from turtle import *
+speed(100)
+color("red")
+width(3)
+forward(150)
+left(90)
+forward(200)
+left(90)
+forward(150)
+left(90)
+forward(200)
+left(90)
+
+forward(45)
+left(90)
+forward(3)
+color("brown")
+forward(60)
+right(90)
+forward(60)
+right(90)
+forward(63)
+
+penup()
+goto(150, 200)
+penup()
+
+
+color("green")
+pendown()
+goto(75, 250)
+pendown
+
+
+pendown()
+goto(0, 200)
+penup()
+
+goto(10, 150)
+pendown()
+color("blue")
+forward(25)
+left(90)
+forward(30)
+left(90)
+color("blue")
+forward(25)
+left(90)
+forward(30)
+left(90)
+
+penup()
+goto(110, 150)
+pendown()
+
+color("blue")
+forward(25)
+left(90)
+forward(30)
+left(90)
+color("blue")
+forward(25)
+left(90)
+forward(30)
+left(90)
+
+
+exitonclick()
