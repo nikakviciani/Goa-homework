9715a172a0757799433c47cdafd4248634d739bc
nikakviciani <kalichavanika55@gmail.com>
2025-02-25 17:45:45 +0400

    commit

---
+++ day4/homework/homework.py
@@ -0,0 +1,8 @@
+mother = "eka"
+father = "eldar"
+big_brother= "luka"
+small_brother= "gabi"
+grandma="galina"
+grandfather="valo"
+family= mother +","+father +","+ big_brother+","+ small_brother +","+ grandfather +","+ grandma
+print(family)
---
+++ day4/homework/homework1.py
@@ -0,0 +1,7 @@
+num1=(str(input))
+num2=(str(input))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
