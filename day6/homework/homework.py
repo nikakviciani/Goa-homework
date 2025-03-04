ba5f6f9d50e9b1d405e7bc113f6eea6c9b5254bb
nikakviciani <kalichavanika55@gmail.com>
2025-03-04 19:03:40 +0400

    commit

---
+++ day6/homework/homework.py
@@ -0,0 +1,5 @@
+name="nika"
+surname="kviciani"
+age="14"
+city="not information"
+print(name +" " +surname+" " +age+" " +city)
---
+++ day6/homework/homework1.py
@@ -0,0 +1,6 @@
+celsius=15
+fahrenheit=(celsius * 9/5)+32
+kelvin=celsius+273.15
+print(celsius)
+print(fahrenheit)
+print(kelvin)
