6439e972603be160b2a66dfbf45de3e3e7d8abee
nikakviciani <kalichavanika55@gmail.com>
2025-03-17 19:25:35 +0400

    commit

---
+++ day10/homework/homework3.py
@@ -0,0 +1,22 @@
+# 4) აიღეთ 3 ცვლადი, შეინახეთ განსხავებული მონაცემთა ტიპები (str,int,float), შემდეგ type ინსტრუქციის გამოყენებით შეამოწმეთ, თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი.
+s1="nika"
+s2="nik"
+s3="nikoloz"
+
+i1=1
+i2=2
+i3=3
+
+f1=1.1
+f2=2.2
+f3=3.3
+
+print(type(s1))
+print(type(s2))
+print(type(s3))
+print(type(i1))
+print(type(i2))
+print(type(i3))
+print(type(f1))
+print(type(f2))
+print(type(f3))
